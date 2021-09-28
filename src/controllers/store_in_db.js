8eca5d4baeaa730abb7245b3766cc9d8c237078e
ahluwaliatikant <atikant.ahluwalia@gmail.com>
2021-09-28 10:42:18 +0530

    firebase new

--- src/controllers/store_in_db.js
+++ src/controllers/store_in_db.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const asyncHandler = require('@joellesenne/express-async-handler')
+const firebase = require("firebase");
 
-exports.storeInDB = asyncHandler((req,res) => {
+const firebaseConfig = {
+  apiKey: "AIzaSyC4L4Hq0gOHWbzPfloqUmk3BivhI2s-bD4",
+  authDomain: "testagent-gjkk.firebaseapp.com",
+  projectId: "testagent-gjkk",
+  storageBucket: "testagent-gjkk.appspot.com",
+  messagingSenderId: "280386602838",
+  appId: "1:280386602838:web:0ab0b044b6b135639d38f7",
+  measurementId: "G-KF0WXL61TM"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+exports.storeInDB = asyncHandler(async(req,res) => {
     const {intentName , parameters , session} = req.body;
 
     console.log(intentName);
@@ -9,6 +24,8 @@ exports.storeInDB = asyncHandler((req,res) => {
     console.log(session);
 
     //TODO make function to upload to firebase
+    const intentCollection = db.collection(intentName);
+    await intentCollection.add(parameters);
     
     res.status(200).json(parameters);
 });
